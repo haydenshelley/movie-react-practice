2f4c0b1a026f55fb9c8d045fac833ef1788cc5c4
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-09-04 15:31:55 -0700

    initial commit

---
+++ src/Content.jsx
@@ -0,0 +1,3 @@
+export function Content() {
+  return <div>Movie App</div>;
+}
---
+++ src/Header.jsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <nav>
+      <a href="#">Home</a> | <a href="#">Random Movie</a>
+    </nav>
+  );
+}
